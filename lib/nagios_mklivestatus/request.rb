4cd3394200a06382525469530f7daa07e8a16a67
alguerillon <alguerillon@gmail.com>
2012-03-07 16:44:40 +0100

    bug request creation

--- lib/nagios_mklivestatus/request.rb
+++ lib/nagios_mklivestatus/request.rb
@@ -24,8 +24,6 @@ class Nagios::MkLiveStatus::Request
   #
   def initialize(path)
     
-    Nagios::MkLiveStatus::init(options)
-    
     #check socket type
     # if the path start with tcp:// => tcp socket
     if path.strip.start_with?("tcp://")
